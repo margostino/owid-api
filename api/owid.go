86ccbf528350f1d9ec464b996ea4f8b9bf8761a3
Martin D'Agostino <martin.dagostino@klarna.com>
2022-05-30 17:08:45 +0200

    make it json response

--- api/owid.go
+++ api/owid.go
@@ -1,12 +1,23 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	currentTime := time.Now().Format(time.RFC850)
-	fmt.Fprintf(w, currentTime)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	resp := make(map[string]string)
+	resp["message"] = "Hello World from Go"
+	resp["language"] = "go"
+	resp["cloud"] = "Hosted on Vercel!"
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		fmt.Println("Error happened in JSON marshal. Err: %s", err)
+	} else {
+		w.Write(jsonResp)
+	}
+	return
 }
